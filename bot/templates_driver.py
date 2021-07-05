8c7bdc19f3d323af9a63dc60930bba7e7655e30b
Ch4zm of Hellmouth <ch4zm.of.hellmouth@gmail.com>
2021-07-05 14:47:51 -0700

    add pennant table classes

--- bot/templates_driver.py
+++ bot/templates_driver.py
@@ -1,4 +1,9 @@
-from templates_championship import PseudoChampionsTable, ToroidalChampionsTable
+from templates_championship import (
+    PseudoChampionsTable,
+    PseudoPenantTable,
+    ToroidalChampionsTable,
+    ToroidalPenantTable,
+)
 from pywikibot import Site
 
 
@@ -7,8 +12,14 @@ if __name__ == "__main__":
     DRY_RUN = False
     site = Site()
 
-    psu = PseudoChampionsTable(site)
-    psu.update(dry_run=DRY_RUN)
+    # psu = PseudoChampionsTable(site)
+    # psu.update(dry_run=DRY_RUN)
 
-    tor = ToroidalChampionsTable(site)
-    tor.update(dry_run=DRY_RUN)
+    # tor = ToroidalChampionsTable(site)
+    # tor.update(dry_run=DRY_RUN)
+
+    psup = PseudoPenantTable(site)
+    psup.update(dry_run=DRY_RUN)
+
+    tsup = ToroidalPenantTable(site)
+    tsup.update(dry_run=DRY_RUN)
